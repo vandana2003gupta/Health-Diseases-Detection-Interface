f4b9a7e577b6f6e7a83820d4c37f9be777c3d533
Vandana Gupta <123993348+vandana2003gupta@users.noreply.github.com>
2025-07-12 13:49:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,15 +45,15 @@ st.markdown("""
         font-size: 16px !important;
     }
 
-    /* Dropdown Arrow & Display Text Styling */
-    .stSelectbox div[role="button"] {
-        background-color: #FFFFFF !important;
-        color: #1e1e1e !important;
-        border: 2px solid #FF4B4B !important;
-        border-radius: 8px !important;
-        padding: 8px !important;
-        font-size: 16px !important;
-    }
+    # /* Dropdown Arrow & Display Text Styling */
+    # .stSelectbox div[role="button"] {
+    #     background-color: #FFFFFF !important;
+    #     color: #1e1e1e !important;
+    #     border: 2px solid #FF4B4B !important;
+    #     border-radius: 8px !important;
+    #     padding: 8px !important;
+    #     font-size: 16px !important;
+    # }
 
     /* Button Styling */
     .stButton>button {
